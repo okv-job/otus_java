ab2ef518bb2b50c526ac40b7cca163e78c26761c
okv <korytnikov.work@gmail.com>
2017-10-16 13:17:58 +0300

    fix

--- Lesson1-1/src/main/java/ru/oleg/korytnikov/CsvToTable.java
+++ Lesson1-1/src/main/java/ru/oleg/korytnikov/CsvToTable.java
@@ -6,46 +6,47 @@ import org.apache.commons.csv.CSVPrinter;
 import java.io.*;
 
 public class CsvToTable {
-
     private final static String PATH_TO_FILE = System.getProperty("user.dir") + "/clients.csv";
 
     public static void main(String... args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter the customer name:");
-        Timer timer = new Timer();
-        new Thread(timer).start();
+        double startTime = System.currentTimeMillis();
         try {
-            String customerName = br.readLine();
-            System.out.println("Enter the company:");
-            String company = br.readLine();
-            System.out.println("Enter phone number:");
-            String phoneNumber = br.readLine();
-            br.close();
-            Customer customer = new Customer(customerName, company, phoneNumber);
+            Customer customer = getCustomer();
+
             File csvData = new File(PATH_TO_FILE);
-            if (csvData.exists()) {
-                FileWriter pw = new FileWriter(PATH_TO_FILE, true);
-                CSVFormat csvFileFormat = CSVFormat.DEFAULT.withRecordSeparator("\n");
-                CSVPrinter csvFilePrinter = new CSVPrinter(pw, csvFileFormat);
-                csvFilePrinter.printRecord(customer.getCustomerCsvData());
-                pw.flush();
-                pw.close();
-            } else {
-                FileWriter pw = new FileWriter(PATH_TO_FILE);
-                CSVFormat csvFileFormat = CSVFormat.DEFAULT.withRecordSeparator("\n");
-                CSVPrinter csvFilePrinter = new CSVPrinter(pw, csvFileFormat);
-                String[] headers = new String[3];
-                headers[0] = "CustomerName";
-                headers[1] = "CustomerCompany";
-                headers[2] = "CustomerPhoneNumber";
-                csvFilePrinter.printRecord(headers);
-                csvFilePrinter.printRecord(customer.getCustomerCsvData());
-                pw.flush();
-                pw.close();
+            boolean newFile = !csvData.exists();
+
+            FileWriter pw = new FileWriter(PATH_TO_FILE, true);
+            CSVFormat csvFileFormat = CSVFormat.DEFAULT.withRecordSeparator("\n");
+            CSVPrinter csvFilePrinter = new CSVPrinter(pw, csvFileFormat);
+            if (newFile) {
+                addHeader(csvFilePrinter);
             }
+            csvFilePrinter.printRecord(customer.getCustomerCsvData());
+            pw.flush();
+            pw.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        timer.stop();
+        System.out.println("The recording was created. It took " +
+                (System.currentTimeMillis() - startTime) / 1000.0 + " seconds.");
+    }
+
+    private static Customer getCustomer() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the customer name:");
+        String customerName = br.readLine();
+        System.out.println("Enter the company:");
+        String company = br.readLine();
+        System.out.println("Enter phone number:");
+        String phoneNumber = br.readLine();
+        br.close();
+        return new Customer(customerName, company, phoneNumber);
+    }
+
+    private static void addHeader(CSVPrinter csvFilePrinter) throws IOException {
+        String[] headers = {"CustomerName", "CustomerCompany", "CustomerPhoneNumber"};
+        csvFilePrinter.printRecord(headers);
     }
 }
