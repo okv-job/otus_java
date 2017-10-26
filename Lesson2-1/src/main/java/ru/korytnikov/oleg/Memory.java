7af4ce4a9037e8cac95b8324f24a3ee6547b9195
okv <korytnikov.work@gmail.com>
2017-10-26 11:04:18 +0300

    fix

--- Lesson2-1/src/main/java/ru/korytnikov/oleg/Memory.java
+++ Lesson2-1/src/main/java/ru/korytnikov/oleg/Memory.java
@@ -2,60 +2,49 @@ package ru.korytnikov.oleg;
 
 public class Memory {
     private long initMemorySize;
+    private long usedMemory;
     private Runtime runtime = Runtime.getRuntime();
 
 
-    private void init(){
+    private void init() {
         System.gc();
         initMemorySize = runtime.totalMemory() - runtime.freeMemory();
     }
 
 
-    private void calculateMemory(){
+    private void calculateMemory() {
+        System.gc();
         long memorySize = runtime.totalMemory() - runtime.freeMemory();
         System.out.print(memorySize - initMemorySize + " bytes ");
     }
 
-    private long calculateMemoryForArray(int size){
-        long memorySize = (runtime.totalMemory() - runtime.freeMemory())/size;
+    private long calculateMemoryForArray(int size) {
+        long memorySize = (runtime.totalMemory() - runtime.freeMemory()) / size;
         return memorySize;
     }
 
 
-    public void getSizeForObject(){
+    public void getSizeForObject() {
         System.out.print("Размер объекта равен: ");
-        for (int i = 0; i < 2; i++){
-            if (i==0){
-                init();
-                new Object();
-                continue;
-            }
-            init();
-            new Object();
-            calculateMemory();
-        }
+        init();
+        Object object = new Object();
+        calculateMemory();
+        object.toString();
         System.out.println();
     }
 
-    public void getSizeForEmptyString(){
+    public void getSizeForEmptyString() {
         System.out.print("Размер пустой строки равен: ");
-        for (int i = 0; i < 2; i++){
-            if (i==0){
-                init();
-                new String();
-                continue;
-            }
-            init();
-            new String();
-            calculateMemory();
-        }
-        System.out.println();
+        init();
+        String string = new String();
+        calculateMemory();
+        System.out.println(string);
     }
 
-    public void getSizeForEmptyIntContainer(){
+    public void getSizeForEmptyIntContainer() {
         System.out.print("Размер пустого int контейнера равен: ");
-        for (int i = 0; i < 2; i++){
-            if (i==0){
+        for (int i = 0; i < 2; i++) {
+            if (i == 0) {
                 init();
                 int[] container = new int[100];
                 continue;
@@ -64,13 +53,13 @@ public class Memory {
             int[] container = new int[100];
             calculateMemory();
         }
-        System.out.println( " и 1 элемент занимает " + calculateMemoryForArray(100) + " bytes");
+        System.out.println(" и 1 элемент занимает " + calculateMemoryForArray(100) + " bytes");
     }
 
-    public void getSizeForEmptyStringContainer(){
+    public void getSizeForEmptyStringContainer() {
         System.out.print("Размер пустого String контейнера равен: ");
-        for (int i = 0; i < 2; i++){
-            if (i==0){
+        for (int i = 0; i < 2; i++) {
+            if (i == 0) {
                 init();
                 String[] container = new String[100];
                 continue;
@@ -79,13 +68,13 @@ public class Memory {
             String[] container = new String[100];
             calculateMemory();
         }
-        System.out.println( " и 1 элемент занимает " + calculateMemoryForArray(100) + " bytes");
+        System.out.println(" и 1 элемент занимает " + calculateMemoryForArray(100) + " bytes");
     }
 
-    public void getSizeForEmptyDoubleContainer(){
+    public void getSizeForEmptyDoubleContainer() {
         System.out.print("Размер пустого double контейнера равен: ");
-        for (int i = 0; i < 2; i++){
-            if (i==0){
+        for (int i = 0; i < 2; i++) {
+            if (i == 0) {
                 init();
                 double[] container = new double[100];
                 continue;
@@ -94,13 +83,13 @@ public class Memory {
             double[] container = new double[100];
             calculateMemory();
         }
-        System.out.println( " и 1 элемент занимает " + calculateMemoryForArray(100) + " bytes");
+        System.out.println(" и 1 элемент занимает " + calculateMemoryForArray(100) + " bytes");
     }
 
-    public void getSizeForEmptyLongContainer(){
+    public void getSizeForEmptyLongContainer() {
         System.out.print("Размер пустого long контейнера равен: ");
-        for (int i = 0; i < 2; i++){
-            if (i==0){
+        for (int i = 0; i < 2; i++) {
+            if (i == 0) {
                 init();
                 long[] container = new long[100];
                 continue;
@@ -109,30 +98,30 @@ public class Memory {
             long[] container = new long[100];
             calculateMemory();
         }
-        System.out.println( " и 1 элемент занимает " + calculateMemoryForArray(100) + " bytes");
+        System.out.println(" и 1 элемент занимает " + calculateMemoryForArray(100) + " bytes");
     }
 
 
-    public void getSizeForFilledIntContainer(int size){
+    public void getSizeForFilledIntContainer(int size) {
         System.out.println("Размер long контейнера равен: ");
-        for (int i = 0; i < size+1; i++){
-            if (i==0){
+        for (int i = 0; i < size + 1; i++) {
+            if (i == 0) {
                 init();
                 int[] container = new int[i];
                 continue;
             }
-            if (i % 5 == 0){
+            if (i % 5 == 0) {
                 System.out.print("при " + i + " элементах ");
                 init();
                 int[] container = new int[i];
-                for (int j = 0; j<i;j++){
+                for (int j = 0; j < i; j++) {
                     container[j] = j;
                 }
                 calculateMemory();
                 System.out.println();
             }
         }
-        System.out.println( "и 1 элемент занимает " + calculateMemoryForArray(100) + " bytes");
+        System.out.println("и 1 элемент занимает " + calculateMemoryForArray(100) + " bytes");
     }
 
 
