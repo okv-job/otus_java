1809538d3944a34731078d86d20d63cd8a74af8c
okv-job <korytnikov.work@gmail.com>
2017-11-12 11:25:34 +0300

    Lesson 4-1

---
+++ Lesson4-1/clean.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#rm logs/*
+rm dumps/*
+mvn clean package
+clear
--- Lesson4-1/src/main/java/ru/korytnikov/oleg/GCTester.java
+++ Lesson4-1/src/main/java/ru/korytnikov/oleg/GCTester.java
@@ -7,10 +7,20 @@ public class GCTester {
     public static void main(String... args) {
         List<Integer> list = new ArrayList<Integer>();
         int i = 1;
-        while (true){
+        int clean = 10;
+        while (true) {
             list.add(i++);
-            if (i%10000000==0){
-                System.out.println(i/10000000);
+            if (i == clean) {
+                List<Integer> backupList = new ArrayList<>(list.subList(0,list.size()/2));
+                list.clear();
+                list = new ArrayList<>(backupList);
+                backupList.clear();
+                clean *= 1.1;
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
